337499b1a43c40976af8a5f1fde7461080256369
baobabnamu <iwantbaobab@gmail.com>
2022-03-11 15:22:07 +0900

    feat: jwt+passport를 활용한 권한 기능 추가

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,24 +3,28 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UserRepository } from './user.repository';
 import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
-    private userRepostiory :UserRepository
+    private userRepostiory :UserRepository,
+    private jwtService: JwtService
   ) {}
 
   signUp(authCrendetialsDto: AuthCredentialsDto): Promise<void> {
     return this.userRepostiory.createUser(authCrendetialsDto);
   }
 
-  async signIn(authCrendetialsDto: AuthCredentialsDto): Promise<string> {
+  async signIn(authCrendetialsDto: AuthCredentialsDto): Promise<{accessToken: string}> {
     const { username, password } = authCrendetialsDto;
     const user = await this.userRepostiory.findOne({username});
 
     if (user && (await bcrypt.compare(password, user.password) )) {
-      return 'Login Success';
+      const payload = {username};
+      const accessToken = await this.jwtService.sign(payload);
+      return { accessToken };
     } else {
       throw new UnauthorizedException('login Failed!');
     }
