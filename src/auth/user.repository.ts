6cd1f6a87ff4f948c4161af08ad92f73057f7471
baobabnamu <iwantbaobab@gmail.com>
2022-03-11 09:35:04 +0900

    feat: 회원가입 중복 예외처리

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,3 +1,4 @@
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 import { EntityRepository, Repository } from "typeorm";
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 import { User } from "./user.entity";
@@ -7,6 +8,15 @@ export class UserRepository extends Repository<User> {
   async createUser(authCrendetialDto: AuthCredentialsDto): Promise<void> {
     const {username, password} = authCrendetialDto;
     const user = this.create({username, password});
-    await this.save(user);
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      if(error.code === '23505') {
+        throw new ConflictException('중복된 유저 이름입니다.');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
