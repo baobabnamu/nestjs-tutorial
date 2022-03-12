ff003daa272415ce32cb1b447dc14c4ad20cf213
baobabnamu <iwantbaobab@gmail.com>
2022-03-13 03:28:47 +0900

    feat: docker volume를 추가하여 개발 환경 개선

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -4,7 +4,7 @@ import * as config from 'config';
 const dbConfig = config.get('db');
 
 export const typeORMConfig : TypeOrmModuleOptions = {
-  type: dbConfig.type,
+  type: 'postgres',
   url: process.env.DATABASE_URL,
   autoLoadEntities: true,
   synchronize: dbConfig.synchronize
